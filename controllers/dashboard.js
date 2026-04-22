625451266a1ee5be1656352200e49039f4873d08
Beckett Brooks <beckett.broox@gmail.com>
2026-04-22 14:02:09 +0100

    Implemented image upload functionality & interaction with Cloudinary

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -70,15 +70,17 @@ const dashboard = {
             date: timestamp,
             songs: [],
         };
-        playlistStore.addPlaylist(newPlaylist);
-        response.redirect('/dashboard');
+        playlistStore.addPlaylist(newPlaylist, request.files.picture, function() {
+            response.redirect('/dashboard');
+        });
     },
 
     deletePlaylist(request, response) {
         const playlistId = request.params.id;
         logger.debug(`Deleting Playlist ${playlistId}`);
-        playlistStore.removePlaylist(playlistId);
-        response.redirect("/dashboard");
+        playlistStore.removePlaylist(playlistId, function() {
+            response.redirect("/dashboard");
+        });
     }
 };
 
--- models/playlist-store.js
+++ models/playlist-store.js
@@ -31,12 +31,31 @@ const playlistStore = {
         this.store.removeItem(this.collection, id, this.array, songId);
     },
 
-    addPlaylist(playlist){
-        this.store.addCollection(this.collection, playlist);
+    async addPlaylist(playlist, file, response){
+        try {
+            playlist.picture = await this.store.addToCloudinary(file);
+            this.store.addCollection(this.collection, playlist);
+            response();
+        } catch (err) {
+            logger.error("Error processing playlist:", err);
+            response(err);
+        }
     },
 
-    removePlaylist(id){
-        this.store.removeCollection(this.collection, this.getPlaylist(id));
+    async removePlaylist(id, response){
+        const playlist= this.getPlaylist(id);
+
+        if(playlist.picture && playlist.picture.public_id){
+            try {
+                await this.store.deleteFromCloudinary(playlist.picture.public_id);
+                logger.info("Cloudinary image deleted");
+            } catch(err){
+                logger.error("Failed to delete Cloudinary image:", err);
+            }
+        }
+
+        this.store.removeCollection(this.collection, playlist);
+        response();
     },
 
     searchPlaylist(search){
